8952f1fc50ff943d97399312d4e2146ec004c2c6
liaofei <136327134@qq.com>
2019-11-28 18:24:20 +0800

    删除无效文件加载

--- src/router/module/index.js
+++ src/router/module/index.js
@@ -1,6 +1,5 @@
 import user from "./user";
 import order from "./order";
 import activity from "./activity";
-import admin from "./admin";
 
-export default [...user, ...order, ...activity, ...admin];
+export default [...user, ...order, ...activity];
